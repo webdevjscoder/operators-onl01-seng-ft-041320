744056cf62d5994cf92506be015774fc3d0fe19e
Juan Cobian <cobianjuan19@gmail.com>
2020-04-13 23:25:37 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,15 @@
-def unsafe?(speed)
-
+def unsafe?(speed= 60)
+  if speed > 60 || speed < 40
+    true
+  else speed == 40 || 60
+    false
+  end
 end
 
 
 
-def not_safe?(speed)
-	
+def not_safe?(speed= 60)
+	speed > 60 || speed < 40 ? true : false
 end
 	
 
